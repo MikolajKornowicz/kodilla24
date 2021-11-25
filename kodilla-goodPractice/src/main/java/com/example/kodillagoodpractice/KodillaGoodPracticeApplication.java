168f809e4409e86e7ebbc2d9f2d47d4d86e7afd0
Mikolaj Kornowicz <mikolaj.kornowicz@gmail.com>
2021-11-25 18:46:20 +0100

    zadanie

---
+++ kodilla-goodPractice/src/main/java/com/example/kodillagoodpractice/KodillaGoodPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.kodillagoodpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodillaGoodPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KodillaGoodPracticeApplication.class, args);
+    }
+
+}
---
+++ kodilla-goodPractice/src/test/java/com/example/kodillagoodpractice/KodillaGoodPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kodillagoodpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodillaGoodPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
