1eaca5eee325a6e7273f61d17c968560c818c1d1
Mikolaj Kornowicz <mikolaj.kornowicz@gmail.com>
2021-12-17 20:14:46 +0100

    pelnawersja

--- kodilla-good-practices/src/main/java/com/kodilla/challenges/OrderRepository.java
+++ kodilla-good-practices/src/main/java/com/kodilla/challenges/OrderRepository.java
@@ -1,20 +1,26 @@
 package com.kodilla.challenges;
 
 import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 
 
 public class OrderRepository {
+
+        LocalDate date = LocalDate.now();
+        LocalTime time = LocalTime.now();
+
 
         public void writeStringToFile(String buyerName, String sellerName, String productName, BigDecimal price) {
 
                 try {
                         BufferedWriter orderData = new BufferedWriter(new FileWriter("order.txt", true));
-                        orderData.write(buyerName+ " bought from " + sellerName + " this product: " + productName + " for: " + price + "\n");
+                        orderData.write(buyerName+ " bought from " + sellerName + " this product: " + productName + " for: " + price + " date: " + date + " " + time.getHour() + ":" + time.getMinute() + ":" + time.getSecond() +  "\n");
                         orderData.close();
                 } catch (IOException e) {
                         System.out.println("Error with saving to file");
