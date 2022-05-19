2a4181535c9d8c56fdd79dd94b6eae6f12962f7c
Mikolaj Kornowicz <mikolaj.kornowicz@gmail.com>
2022-05-19 21:27:36 +0200

    zadanie30.5fix

--- kodilla-jdbc/src/test/java/com/kodilla/jdbc/StoredProcTestSuite.java
+++ kodilla-jdbc/src/test/java/com/kodilla/jdbc/StoredProcTestSuite.java
@@ -6,6 +6,8 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class StoredProcTestSuite {
 
     @Test
@@ -15,23 +17,22 @@ public class StoredProcTestSuite {
         String sqlUpdate = "UPDATE READERS SET VIP_LEVEL=\"Not set\"";
         Statement statement = dbManager.getConnection().createStatement();
         statement.executeUpdate(sqlUpdate);
-        String sqlCheckTable = "SELECT COUNT(*) AS HOW_MANY FROM READERS WHERE VIP_LEVEL=\"Not set\"";
-        ResultSet rs = statement.executeQuery(sqlCheckTable);
 
         // When
         String sqlProcedureCall = "CALL UpdateVipLevels()";
         statement.execute(sqlProcedureCall);
 
         // Then
-        /*
+        String sqlCheckTable = "SELECT COUNT(*) AS HOW_MANY FROM READERS WHERE VIP_LEVEL=\"Not set\"";
+        ResultSet rs = statement.executeQuery(sqlCheckTable);
         int howMany = -1;
         if(rs.next()){
-            howMany= rs.getInt("HOW MANY");
+            howMany= rs.getInt("HOW_MANY");
         }
         assertEquals(0, howMany);
         rs.close();
         statement.close();
-         */
+
     }
 
     @Test
