4702beae41387f5e9c51a2bf5d39bc992b1afbb3
Mikolaj Kornowicz <mikolaj.kornowicz@gmail.com>
2022-05-24 16:15:43 +0200

    zadanie31.1

--- kodilla-testing2/src/main/java/com/kodilla/testing2/facebook/FacebookTestingApp.java
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/facebook/FacebookTestingApp.java
@@ -1,2 +1,31 @@
-package com.kodilla.testing2.facebook;public class FacebookTestingApp {
+package com.kodilla.testing2.facebook;
+
+import com.kodilla.testing2.config.WebDriverConfig;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+public class FacebookTestingApp {
+
+    public static final String XPATH_BUTTON ="//div[@class=\"_6ltg\"]/a";
+    public static final String XPATH_ACCEPT = "//div[@class=\"_9xo5\"]/button[1]";
+    public static final String XPATH_DAY = "//select[contains(@name, 'birthday_day')]";
+
+
+
+    public static void main(String[] args) {
+        WebDriver driver = WebDriverConfig.getDriver(WebDriverConfig.CHROME);
+        driver.get("https://facebook.com");
+
+        WebElement accept = driver.findElement(By.xpath(XPATH_ACCEPT));
+        accept.click();
+
+        WebElement newAcc = driver.findElement(By.xpath(XPATH_BUTTON));
+        newAcc.click();
+
+        WebElement day = driver.findElement(By.xpath(XPATH_DAY));
+        Select selectDay = new Select(day);
+        selectDay.selectByIndex(1);
+    }
 }
